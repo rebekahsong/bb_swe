5db97396533d71f79be840d6cfef970055ad2ae9
Ben Bushnell <bbushnell@Bens-MacBook-Pro.local>
2021-04-16 15:48:54 -0400

    added Django REST authentication to the backend

--- musey/urls.py
+++ musey/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path, include
 from . import views
+from django.contrib import admin
+
 
 urlpatterns = [
     path('', views.mainView, name='index'),
     path('api', views.songs_list),
     path('api/songs', views.songs_list),
-    path('api/songs/<str:song_title>', views.song_detail)
+    path('api/songs/<str:song_title>', views.song_detail),
 ]
