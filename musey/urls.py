3b4488efcf7f722a13f4587343fb4b363c444e8d
Ben Bushnell <bbushnell@Bens-MacBook-Pro.local>
2021-05-19 00:34:50 -0400

    react native stuff

--- musey/urls.py
+++ musey/urls.py
@@ -1,13 +1,24 @@
 from django.urls import path, re_path, include
 from . import views
+from .views import CreateUserAPIView, LogoutUserAPIView
 from django.contrib import admin
+from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
     path('api', views.songs_list),
     path('api/songs', views.songs_list),
     path('api/ratings', views.ratings),
-    path('api/setuser', views.setuser),
+    # path('api/setuser', views.setuser),
+    re_path(r'^api/login/$',
+        obtain_auth_token,
+        name='auth_user_login'),
+    re_path(r'^api/register/$',
+        CreateUserAPIView.as_view(),
+        name='auth_user_create'),
+    re_path(r'^api/logout/$',
+        LogoutUserAPIView.as_view(),
+        name='auth_user_logout'),
     path('api/ratings/<str:usernameSong>', views.deleteRating),
     path('api/songs/<str:song_title>', views.song_detail),
 ]
